82739807a84e72b8b517c5f6abf6983e2b866c61
Marzaniq <161853646+0xmarzaniq@users.noreply.github.com>
2024-03-01 22:45:31 +0700

    Added calculator

---
+++ calculator.js
@@ -0,0 +1,21 @@
+var CALC = {
+
+    add: function(a, b) {
+        return a + b;
+    },
+
+    sub: function(a, b) {
+        return a - b;
+    },
+
+    mul: function(a, b) {
+        return a * b;
+    },
+
+    div: function(a, b) {
+        return a / b;
+    }
+
+};
+
+module.exports = CALC;
